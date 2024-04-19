12e192f06779b4e65465ca708cb6573c8dc13f4e
Dnjomou Loic <dnjomouloic@gmail.com>
2024-04-19 10:12:02 +0100

    audio

--- models/survey_question.py
+++ models/survey_question.py
@@ -7,6 +7,12 @@ class SurveyQuestion(models.Model):
     _inherit = 'survey.question'
 
     question_image = fields.Image('Image', max_width=1024, max_height=1024)
-    question_audio = fields.Binary(string='Audio')
     question_image_filename = fields.Char('Image Filename')
-    question_audio_filename = fields.Char('Audio Filename')
+
+    question_audios = fields.Many2many(
+        comodel_name='ir.attachment',
+        relation='survey_question_audio_rel',
+        column1='question_id',
+        column2='attachment_id',
+        string='Audios'
+    )
